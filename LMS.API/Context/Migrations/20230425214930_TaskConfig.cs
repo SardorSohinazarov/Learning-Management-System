eb022279eb5fa888109264067d34a8f7c92f1215
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-04-26 13:48:59 +0500

    Model Configurations Implemented

---
+++ LMS.API/Context/Migrations/20230425214930_TaskConfig.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS.API.Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class TaskConfig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskComments_Tasks_TaskId",
+                table: "TaskComments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Courses_CourseId",
+                table: "Tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTasks_Tasks_TaskId",
+                table: "UserTasks");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tasks",
+                table: "Tasks");
+
+            migrationBuilder.RenameTable(
+                name: "Tasks",
+                newName: "tasks");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Tasks_CourseId",
+                table: "tasks",
+                newName: "IX_tasks_CourseId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "tasks",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "TaskName",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_tasks",
+                table: "tasks",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskComments_tasks_TaskId",
+                table: "TaskComments",
+                column: "TaskId",
+                principalTable: "tasks",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tasks_Courses_CourseId",
+                table: "tasks",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTasks_tasks_TaskId",
+                table: "UserTasks",
+                column: "TaskId",
+                principalTable: "tasks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskComments_tasks_TaskId",
+                table: "TaskComments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_tasks_Courses_CourseId",
+                table: "tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTasks_tasks_TaskId",
+                table: "UserTasks");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_tasks",
+                table: "tasks");
+
+            migrationBuilder.RenameTable(
+                name: "tasks",
+                newName: "Tasks");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_tasks_CourseId",
+                table: "Tasks",
+                newName: "IX_Tasks_CourseId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Tasks",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldDefaultValue: "TaskName");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tasks",
+                table: "Tasks",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskComments_Tasks_TaskId",
+                table: "TaskComments",
+                column: "TaskId",
+                principalTable: "Tasks",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Courses_CourseId",
+                table: "Tasks",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTasks_Tasks_TaskId",
+                table: "UserTasks",
+                column: "TaskId",
+                principalTable: "Tasks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ LMS.API/Context/Migrations/20230426084807_ModelsConfig.cs
@@ -0,0 +1,322 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS.API.Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelsConfig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                table: "AspNetUserClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                table: "AspNetUserLogins");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserRoles_AspNetUsers_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskComments_AspNetUsers_UserId",
+                table: "TaskComments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_tasks_Courses_CourseId",
+                table: "tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserCourses_AspNetUsers_UserId",
+                table: "UserCourses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserCourses_Courses_CourseId",
+                table: "UserCourses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTasks_AspNetUsers_UserId",
+                table: "UserTasks");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Courses",
+                table: "Courses");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AspNetUsers",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameTable(
+                name: "Courses",
+                newName: "courses");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUsers",
+                newName: "users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "courses",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_courses",
+                table: "courses",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_users",
+                table: "users",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserClaims_users_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserLogins_users_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserRoles_users_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_users_UserId",
+                table: "AspNetUserTokens",
+                column: "UserId",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskComments_users_UserId",
+                table: "TaskComments",
+                column: "UserId",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tasks_courses_CourseId",
+                table: "tasks",
+                column: "CourseId",
+                principalTable: "courses",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserCourses_courses_CourseId",
+                table: "UserCourses",
+                column: "CourseId",
+                principalTable: "courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserCourses_users_UserId",
+                table: "UserCourses",
+                column: "UserId",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTasks_users_UserId",
+                table: "UserTasks",
+                column: "UserId",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserClaims_users_UserId",
+                table: "AspNetUserClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserLogins_users_UserId",
+                table: "AspNetUserLogins");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserRoles_users_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_users_UserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskComments_users_UserId",
+                table: "TaskComments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_tasks_courses_CourseId",
+                table: "tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserCourses_courses_CourseId",
+                table: "UserCourses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserCourses_users_UserId",
+                table: "UserCourses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTasks_users_UserId",
+                table: "UserTasks");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_courses",
+                table: "courses");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_users",
+                table: "users");
+
+            migrationBuilder.RenameTable(
+                name: "courses",
+                newName: "Courses");
+
+            migrationBuilder.RenameTable(
+                name: "users",
+                newName: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Courses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Courses",
+                table: "Courses",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AspNetUsers",
+                table: "AspNetUsers",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserRoles_AspNetUsers_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "AspNetUserTokens",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskComments_AspNetUsers_UserId",
+                table: "TaskComments",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tasks_Courses_CourseId",
+                table: "tasks",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserCourses_AspNetUsers_UserId",
+                table: "UserCourses",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserCourses_Courses_CourseId",
+                table: "UserCourses",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTasks_AspNetUsers_UserId",
+                table: "UserTasks",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
